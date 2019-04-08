717ce4d35b26060fb5df58ec3f2a764256090c20
Daniel <Daniel@CUNYs-MBP-2.home>
2019-04-08 11:14:55 -0400

    changed test transaction services file name

--- server/app/tests/test_transaction_service.py
+++ server/app/tests/test_transaction_service.py
@@ -3,16 +3,16 @@ from services import transaction
 from app_factories import create_app_test
 
 
-class TestMakeTransaction(unittest.TestCase):
+class TestTransaction(unittest.TestCase):
     def test_make_purchase(self):
         app = create_app_test('config.DefaultConfig')
         app.app_context().push()
-        transaction.make_purchase("aapl", 11, 1)
+        transaction.make_purchase("aapl", 11, 7)
     
     def test_get_num_stocks(self):
         app = create_app_test('config.DefaultConfig')
         app.app_context().push()
-        ns = transaction.get_num_stocks('AAPL', 1)
+        ns = transaction.get_num_stocks('AAPL', 7)
         print(ns)
         self.assertTrue(ns > 0)
 
