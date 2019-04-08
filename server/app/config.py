61acd6351cdd8fc8f59a400e963e0bfba47f7bf8
Daniel <Daniel@CUNYs-MBP-2.home>
2019-04-07 21:17:15 -0400

    fixed sign out button and config file

--- server/app/config.py
+++ server/app/config.py
@@ -8,15 +8,16 @@ class DefaultConfig(object):
     USER_ENABLE_EMAIL = True
     USER_ENABLE_USERNAME = False
     USER_REQUIRE_RETYPE_PASSWORD = False
-    #USER_ALLOW_LOGIN_WITHOUT_CONFIRMED_EMAIL = True
     USER_LOGOUT_URL = '/sign-out'
 
     USER_EMAIL_SENDER_EMAIL = 'helpdesk@webstocks.com'
 
     SECRET_KEY = 'Insecure Development Key asdoincpqoiejrpqoiwejrpqoiejrpqowiejpoimpxozicjzxpcivj'
 
-    MAIL_SERVER : 'smtp.gmail.com'
-    MAIL_PORT : 465
-    MAIL_USE_SSL : True
-    MAIL_USERNAME : 'danieljladner@gmail.com'
-    MAIL_PASSWORD : "oekoysjwrlbeoobw"
+    MAIL_SERVER='smtp.gmail.com',
+    MAIL_PORT=587,
+    MAIL_USE_TLS=True,
+    MAIL_USERNAME = 'danieljladner@gmail.com',
+    MAIL_PASSWORD = "kjgtlwperueccznq"
+
+    DEBUG=True
